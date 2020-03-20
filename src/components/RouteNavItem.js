0519cfff08f7f6781e9124d397dab00561b6ded2
lijinhuit <254358087@qq.com>
2020-03-20 11:55:47 +0800

    no message

--- src/components/RouteNavItem.js
+++ src/components/RouteNavItem.js
@@ -6,12 +6,13 @@ export default props =>
   <Route
     path={props.href}
     exact
-    children={({ match, history }) =>
-      {/*<NavItem*/}
-        {/*onClick={e => history.push(e.currentTarget.getAttribute("href"))}*/}
-        {/*{...props}*/}
-        {/*active={match ? true : false}*/}
-      {/*>*/}
-        {/*{props.children}*/}
-      {/*</NavItem>}*/}
+   
   />;
+  // children={({ match, history }) =>
+  // {/*<NavItem*/}
+  //   {/*onClick={e => history.push(e.currentTarget.getAttribute("href"))}*/}
+  //   {/*{...props}*/}
+  //   {/*active={match ? true : false}*/}
+  // {/*>*/}
+  //   {/*{props.children}*/}
+  // {/*</NavItem>}*/}
